ea7aeaf8105343f528ebfd89d68a69dad00550c1
crispthoalex <alex877@gmail.com>
2020-06-26 14:56:27 +0000

    Add task 4

--- models/base_model.py
+++ models/base_model.py
@@ -12,10 +12,20 @@ class BaseModel():
     A class that defines all common attributes/methods for other classes
     """
 
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.utcnow()
-        self.updated_at = datetime.utcnow()
+    def __init__(self, *args, **kwargs):
+        """
+        A method constructor of BaseModel. Define attributtes of Class parent
+        """
+        if len(kwargs) == 0:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.utcnow()
+            self.updated_at = datetime.utcnow()
+        else:
+            for key,value in kwargs.items():
+                if key in ["created_at", "updated_at"]:
+                    setattr(self, key, datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
+                elif key not in ['__class__']:
+                    setattr(self, key, value)
 
     def __str__(self):
         """
