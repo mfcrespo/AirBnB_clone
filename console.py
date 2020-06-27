f0a7a4d38759dbd2a714cebf87a20bad68ab2c14
mfcrespo <mafe_crespo@hotmail.com>
2020-06-27 18:09:47 +0000

    console.py

--- console.py
+++ console.py
@@ -3,7 +3,9 @@
 A module cmd that contains the entry point of the command interpreter
 """
 import cmd
-
+import models
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
 
 class HBNBCommand(cmd.Cmd):
     """
@@ -14,13 +16,13 @@ class HBNBCommand(cmd.Cmd):
 
     def do_quit(self, arg):
         """
-        A method to exit of the program
+        A command to exit of the program
         """
         return True
 
     def do_EOF(self, arg):
         """
-        A method to exit of the program
+        A command to exit of the program
         """
         print()
         return True
@@ -30,6 +32,41 @@ class HBNBCommand(cmd.Cmd):
         A method that when an empty line + ENTER shouldn’t execute anything
         """
         pass
+
+    def do_create(self, arg):
+        """ A command that creates a new instance of BaseModel,
+        saves it (to the JSON file) and prints the id
+        """
+        if not arg:
+            print("** class name missing **")
+        elif arg not in "BaseModel":
+            print("** class doesn't exist **")
+        else:
+            new_ins = BaseModel()  #Create new instance
+            new_ins.save()
+            print(new_ins.id)
+
+    def do_show(self, arg):
+        """ A command that show the class name and id
+        """
+        list = arg.split()        
+        if len(list) == 0:
+            print("** class name missing **")
+        elif list[0] not in "BaseModel":
+            print("** class doesn't exist **")
+        elif len(list) == 1:
+            print("** instance id missing **")
+        else:
+            key ="{}.{}".format(list[0], list[1])
+            temp = models.storage.all()  # temp is self.__object
+            if key in temp:
+                print (temp[key])
+            else:
+                print ("** no instance found **") 
+        
+
+            #print("** instance id missing **")
+        #elif 
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
