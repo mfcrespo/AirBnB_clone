677b0c1a32c5ab010c0fbea08f611f7d98e4d771
crispthoalex <alex877@gmail.com>
2020-06-28 01:31:31 +0000

    Add task 7: update console, more commands

--- console.py
+++ console.py
@@ -6,6 +6,7 @@ import cmd
 import models
 from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
+import shlex
 
 
 class HBNBCommand(cmd.Cmd):
@@ -50,15 +51,15 @@ class HBNBCommand(cmd.Cmd):
     def do_show(self, arg):
         """ A command that show the class name and id
         """
-        list = arg.split()
-        if len(list) == 0:
+        l_arg = shlex.split(arg)
+        if len(l_arg) == 0:
             print("** class name missing **")
-        elif list[0] not in "BaseModel":
+        elif l_arg[0] not in "BaseModel":
             print("** class doesn't exist **")
-        elif len(list) == 1:
+        elif len(l_arg) == 1:
             print("** instance id missing **")
         else:
-            key = "{}.{}".format(list[0], list[1])
+            key = "{}.{}".format(l_arg[0], l_arg[1])
             temp = models.storage.all()  # temp is self.__object
             if key in temp:
                 print(temp[key])
@@ -68,20 +69,62 @@ class HBNBCommand(cmd.Cmd):
     def do_destroy(self, arg):
         """ A command that show the class name and id
         """
-        list = arg.split()
-        if len(list) == 0:
+        l_arg = shlex.split(arg)
+        if len(l_arg) == 0:
             print("** class name missing **")
-        elif list[0] not in "BaseModel":
+        elif l_arg[0] not in "BaseModel":
             print("** class doesn't exist **")
-        elif len(list) == 1:
+        elif len(l_arg) == 1:
             print("** instance id missing **")
         else:
-            key = "{}.{}".format(list[0], list[1])
+            key = "{}.{}".format(l_arg[0], l_arg[1])
             temp = models.storage.all()  # temp is self.__object
             if key in temp:
                 del (temp[key])
             else:
                 print("** no instance found **")
+
+    def do_all(self, arg):
+        """
+        A command prints all string representation of all instances based or
+        not on the class name
+        """
+        l_arg = shlex.split(arg)
+        list_obj = []
+        temp = models.storage.all()  # temp is self.__object
+
+        if (arg and l_arg[0] in "BaseModel") or len(l_arg) == 0:
+            for key, value in temp.items():
+                list_obj.append(str(value))  # str(temp[key])
+            print(list_obj)
+        else:
+            print("** class doesn't exist **")
+
+    def do_update(self, arg):
+        """
+        A command that Updates an instance based on the class name and
+        id by adding or updating attribute
+        """
+        l_arg = shlex.split(arg)
+        if len(l_arg) == 0:
+            print("** class name missing **")
+        elif l_arg[0] not in "BaseModel":
+            print("** class doesn't exist **")
+        elif len(l_arg) == 1:
+            print("** instance id missing **")
+        else:
+            key = "{}.{}".format(l_arg[0], l_arg[1])
+            temp = models.storage.all()  # temp is self.__object
+            if key not in temp:
+                print("** no instance found **")
+            elif len(l_arg) == 2:
+                print("** attribute name missing **")
+            elif len(l_arg) == 3:
+                print("** value missing **")
+            else:
+                new_obj = temp.get(key)
+                setattr(new_obj, l_arg[2], l_arg[3])
+                models.storage.save()
 
 
 if __name__ == '__main__':
