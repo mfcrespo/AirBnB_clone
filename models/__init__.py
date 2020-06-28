b2d88c4d1134e853463aaa7fec090ea9e4129987
mfcrespo <mafe_crespo@hotmail.com>
2020-06-28 16:12:25 +0000

    Create dict_class

--- models/__init__.py
+++ models/__init__.py
@@ -3,7 +3,10 @@
 Update this file. Creating a unique Filestorage instance
 """
 from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
+from models.user import User
 
+dict_class = {'BaseModel': BaseModel, 'User': User}
 
 storage = FileStorage()
 storage.reload()
