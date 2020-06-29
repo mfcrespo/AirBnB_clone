49f4a428f985919a0088c5e36e1a7842222ae6da
crispthoalex <alex877@gmail.com>
2020-06-29 23:48:35 +0000

    Add Amenity test case

---
+++ tests/test_models/test_amenity.py
@@ -0,0 +1,99 @@
+#!/usr/bin/python3
+"""
+Unittest Amenity class
+"""
+import unittest
+import pep8
+import sys
+from datetime import datetime
+from models import amenity
+from models.amenity import Amenity
+
+
+class TestPep8B(unittest.TestCase):
+    """ Test pep8 style validation """
+    def test_pep8(self):
+        """ test base and test_base for pep8 conformance """
+        style = pep8.StyleGuide(quiet=True)
+        file1 = 'models/amenity.py'
+        file2 = 'tests/test_models/test_amenity.py'
+        result = style.check_files([file1, file2])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warning).")
+
+
+class TestDocsB(unittest.TestCase):
+    """ check for documentation """
+    def test_module_doc(self):
+        """ check for module documentation """
+        self.assertTrue(len(amenity.__doc__) > 0)
+
+    def test_class_doc(self):
+        """ check for documentation """
+        self.assertTrue(len(Amenity.__doc__) > 0)
+
+    def test_method_docs(self):
+        """ check for method documentation """
+        for func in dir(Amenity):
+            self.assertTrue(len(func.__doc__) > 0)
+
+
+class BaseModelclassTests(unittest.TestCase):
+    """ Test Case for base_model moudle """
+
+    def setUp(self):
+        """ Create instance global  """
+        self.ins0 = Amenity()
+        self.ins1 = Amenity()
+
+    def tearDown(self):
+        """ Clean All test case """
+        pass
+
+    def test_instance(self):
+        """ Test Case to check instance  """
+        self.assertIsInstance(self.ins0, Amenity)
+        self.assertIsInstance(self.ins1, Amenity)
+
+    def test_id(self):
+        """
+        Test id of instances
+        instance id will be not equal
+        type, will be str
+        """
+        self.assertNotEqual(self.ins0.id, self.ins1.id)
+        self.assertEqual(type(self.ins0.id), str)
+        self.assertEqual(type(self.ins1.id), str)
+
+    def test_datetime_save(self):
+        """ Test datetime to compare format """
+        cre = self.ins0.created_at
+        self.ins0.save()
+        up = self.ins0.updated_at
+        self.assertEqual(type(cre), datetime)
+        self.assertEqual(type(up), datetime)
+        self.assertNotEqual(cre, up)  # time create and update are diff
+
+    def test_to_dict(self):
+        """ The value return by to_dict the same value """
+        dateform = '%Y-%m-%dT%H:%M:%S.%f'
+        dic = self.ins0.to_dict()
+        self.assertEqual(type(dic['created_at']), str)
+        self.assertEqual(type(dic['updated_at']), str)
+        self.assertEqual(dic['created_at'],
+                         self.ins0.created_at.strftime(dateform))
+        self.assertEqual(dic['updated_at'],
+                         self.ins0.updated_at.strftime(dateform))
+
+    def test_name_str(self):
+        """ Test data type intro. Will be str """
+        namestr = self.ins0.name
+        self.assertEqual(type(namestr), str)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+# Run Test
+# python3 -m unittest discover tests
+# python3 -m unittest tests/test_models/test_amenity.py
